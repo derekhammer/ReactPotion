78ea2baa544d9233ea1163031156ae80b9edd380
Derek <derekhammer@hotmail.com>
2018-11-28 15:01:14 -0800

    looping potions, bo styling yet

--- src/components/PotionList.jsx
+++ src/components/PotionList.jsx
@@ -1,15 +1,47 @@
 import React from "react";
 import Potion from "./Potion";
 
+var masterPotionList = [
+    {
+        name: 'Titan Grip',
+        ingredient: 'Dragon Blood',
+        modifier: 'Strength',
+        stock: 32
+    },
+    {
+        name: 'Mind Tap',
+        ingredient: 'Mana Wraith Essence',
+        modifier: 'Intelligence',
+        stock: 17
+    },
+    {
+        name: 'Cobra Strike',
+        ingredient: 'Purified Felwater',
+        modifier: 'Agility',
+        stock: 10
+    },
+    {
+        name: 'Goblin Tea',
+        ingredient: 'Gildroot',
+        modifier: 'Luck',
+        stock: 2
+    }
+    
+]
+
 function PotionList(){
     return(
         <div>
-        <Potion
-        name="Titan Grip"
-        ingredient="Dragon Blood"
-        modifier="Strength"
-        stock="32"
-        />
+            <hr/>
+            {masterPotionList.map((potion, index) =>
+            <Potion name={potion.name}
+                ingredient={potion.ingredient}
+                modifier={potion.modifier}
+                stock={potion.stock}
+                key={index}          
+            />        
+                )}
+        
         </div>
     );
 }
