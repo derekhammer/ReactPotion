bfd2699244b0d6694b43daa5681e8800875d1c54
Derek <derekhammer@hotmail.com>
2018-11-28 15:46:00 -0800

    add buttons in header to routes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 function Header(){
   return (
+      <div>
     <h1>Potion Shop</h1>
+    <Link to="/">Home</Link> | <Link to="/newpicket">Craft Potion</Link>
+    </div>
   );
 }
 
