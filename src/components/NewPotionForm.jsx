ab5a7dd0c968b3f8d1a5c97e4a657d2b4bb275ed
Derek <derekhammer@hotmail.com>
2018-12-05 10:43:55 -0800

    able to console log inputs from the potion form

--- src/components/NewPotionForm.jsx
+++ src/components/NewPotionForm.jsx
@@ -1,25 +1,43 @@
 import React from 'react';
 
 function NewPotionForm(){
+  let _name = null;
+  let _ingredient = null;
+  let _modifier = null;
+  let _stock = null;
+
+  function handleNewPotionFormSubmission(event) {
+    event.preventDefault();
+    console.log(_name.value);
+    console.log(_ingredient.value);
+    console.log(_modifier.value);
+    console.log(_stock.value);
+  }
+
   return(
     <div>
-      <form>
+      <form onSubmit={handleNewPotionFormSubmission}>
         <input
           type='text'
           id='name'
-          placeholder='New Potion' />
+          placeholder='New Potion'
+          ref={(input) => {_name = input;}} />
         <input
           type='text'
           id='ingredient'
-          placeholder='Main Ingredient' />
+          placeholder='Main Ingredient'
+          ref={(input) => {_ingredient = input;}} />
         <input
           type='text'
           id='modifier'
-          placeholder='Stat Modified' />
+          placeholder='Stat Modified'
+          ref={(input) => {_modifier = input;}} />
         <input
           type='number'
           id='stock'
-          placeholder='In Stock' />
+          placeholder='In Stock'
+          ref={(input) => {_stock = input;}} />
+          <button type='submit'>Brew!</button>
       </form>
     </div>
   );
